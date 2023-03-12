cb690136af3b5512094b33b65361cf8885d194fb
Dylan Shine <dyshiney11@gmail.com>
2023-03-12 14:24:55 -0400

    aa

--- GDS_126_Engine/js/GameStates/menu.js
+++ GDS_126_Engine/js/GameStates/menu.js
@@ -2,7 +2,7 @@
 This file contains all of the code for the Main Menu
 ----------------------------------*/
 
-var ctx = canvas.getContext(`2d`);
+
 
 
 
@@ -15,17 +15,20 @@ menuBackground.img.src = "images/MenuBackgroundSmall1024x512 VERSION 2.png"
 menuBackground.width=canvas.width
 menuBackground.height=canvas.height
 
-var startButton = new GameObject({width:100, height:50});
+var startButton = new GameObject({/*width:100, height:50*/});
 startButton.img.src="images/StartButton.jpg"
-startButton.width=canvas.width/2
-startButton.height=canvas.height/2
-startButton.hitBoxWidth=800
+startButton.width=100
+startButton.height=50
+startButton.hitBoxWidth=100  //800
 
-ctx.font = "40px Georgia"
-ctx.fillStyle = "black";
-ctx.fillText("BEGIN", canvas.width/2, canvas.height/2-100);
+
 
 gameStates[`menu`] =function(){
+	
+	var ctx = canvas.getContext(`2d`);
+	ctx.font = "40px Georgia"
+	ctx.fillStyle = "black";
+	ctx.fillText("BEGIN", canvas.width/2, canvas.height/2-100);
 
 	//Makes the button clickable
 	if(startButton.overlap(mouse))
